036a9e4ee91f509034741c2ab90bc9cff5c12a8d
Tyler Morris Ford <tylerford@gmail.com>
2020-04-14 18:08:55 -0600

    add brand style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import styled from 'styled-components'
 import './App.css';
+
+const Callout = styled.span`
+  margin-bottom: 0;
+  font-size: 0.8em;
+  font-weight: 200;
+`
+
+const Brand = styled.h3`
+  margin-top: 2px;
+`
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <header className="App-header">
-        <h3>Hello, World!</h3>
+        <Callout>Welcome to</Callout>
+        <Brand>ThisApp!</Brand>
       </header>
     </div>
   );
