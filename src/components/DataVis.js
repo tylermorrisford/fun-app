a28372cee879afb3473a15c5dd598ecf36089c85
Tyler Morris Ford <tylerford@gmail.com>
2020-04-25 18:48:19 -0600

    add daily utah data url

--- src/components/DataVis.js
+++ src/components/DataVis.js
@@ -12,6 +12,7 @@ font-weight: 200;
 const Brand = styled.h3`
 margin-top: 2px;
 `
+const historicalUtahJson = 'https://covidtracking.com/api/v1/states/ut/daily.json';
 
 const DataVis = () => {
   const data = [{name: '3/7', positive: 1}, {name: '3/14', positive: 6}, {name: '3/21', positive: 136}, {name: '3/28', positive: 602}, {name: 'April 4', positive: 1428}, {name: '4/11', positive: 2206}, {name: '4/18', positive: 2931}, {name: '4/24', positive: 3782}];
@@ -30,7 +31,7 @@ const DataVis = () => {
         <Callout>this is</Callout>
         <Brand>DataVis</Brand>
         <br/>
-        <h4>Confirmed positive Covid-19 cases in Utah</h4>
+        <h4>Positive Covid-19 cases in Utah</h4>
         {renderLineChart}
       </header>
     )
