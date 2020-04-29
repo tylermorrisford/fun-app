021b832e752fd4abfbca0535029124ab95ea2e20
Tyler Morris Ford <tylerford@gmail.com>
2020-04-28 22:44:16 -0600

    add copy

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,20 @@ import React from 'react';
 import styled from 'styled-components'
 import '../App.css';
 
+const Home = () => {
+    return(
+        <header className="App-header">
+        <Callout>Welcome to</Callout>
+        <Brand>ThisApp!</Brand>
+        <Text><Words>ThisApp is just here to keep us (us === me) company during the 'great Quarantine of 2020', due to the Covid-19 pandemic. 
+         <br/>Does one capitalize 'pandemic'? I could google it, but... will I? Anywho, I hope you're safe. If you're wondering how many cases 
+         of the novel coronavirus there are in my state, click the data-vis link for a handy chart.</Words></Text>
+      </header>
+    )
+}
+
+export default Home;
+
 const Callout = styled.span`
   margin-bottom: 0;
   font-size: 0.8em;
@@ -12,15 +26,12 @@ const Brand = styled.h3`
   margin-top: 2px;
 `
 
-const Home = () => {
-    return(
-        <header className="App-header">
-        <Callout>Welcome to</Callout>
-        <Brand>ThisApp!</Brand>
-        <p>ThisApp is just here to keep us company during the 'great Quarantine of 2020' due to the Covid-19 pandemic. 
-          Does one capitalize 'pandemic'? No way of knowing I guess. </p>
-      </header>
-    )
-}
+const Text = styled.div`
+  margin: 0 auto:
+  padding: 0 25px;
+  width: 80%;
+`
 
-export default Home;
+const Words = styled.p`
+font-size: 0.6em;
+`
